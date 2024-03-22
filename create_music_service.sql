5b881c0a5e5baa4c57f6970395c852e89b8b89b9
Bess096 <123263059+Bess096@users.noreply.github.com>
2024-03-22 23:52:20 +0300

    Update create.sql

--- create_music_service.sql
+++ create_music_service.sql
@@ -8,8 +8,9 @@
    CREATE TABLE IF NOT EXISTS album (
       id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
       title VARCHAR(100) NOT NULL,
-      release_year INT NOT NULL
-);
+      release_year INT NOT NULL CHECK (release_year <= 2024),
+      CHECK (release_year > 1900)
+   );
 -- Создание таблицы artists_of_album
    CREATE TABLE IF NOT EXISTS artists_of_album (
 	  artist_id INT NOT NULL,
@@ -17,20 +18,20 @@
 	  album_id INT NOT NULL,
     CONSTRAINT fk_album_id FOREIGN KEY (album_id) REFERENCES album (id),
 	  PRIMARY KEY (artist_id, album_id)
-);
+   );
 -- Создание таблицы track
    CREATE TABLE IF NOT EXISTS track (
       id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       name VARCHAR(150) NOT NULL,
-      length timestamp without time zone NOT NULL,
-	  album_id INT NOT NULL,
+      length INT NOT NULL,
+      album_id INT NOT NULL,
      CONSTRAINT fk_album_id FOREIGN KEY (album_id) REFERENCES album (id)
-);
+   );
 -- Создание таблицы genre
    CREATE TABLE IF NOT EXISTS genre (
       id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-      name VARCHAR(150) NOT NULL
-);
+      name VARCHAR(150) NOT NULL UNIQUE
+   );
 -- Создание таблицы genres_of_artist
    CREATE TABLE IF NOT EXISTS genres_of_artist (
 	  artist_id INT NOT NULL,
@@ -38,13 +39,14 @@
 	   genre_id INT NOT NULL,
     CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genre (id),
 	   PRIMARY KEY (artist_id, genre_id)
-);
+   );
 -- Создание таблицы digest
    CREATE TABLE IF NOT EXISTS digest (
       id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       name VARCHAR(150) NOT NULL,
-	  release_year INT NOT NULL
-);
+      release_year INT NOT NULL CHECK (release_year <= 2024),
+      CHECK (release_year > 1900)
+   );
 -- Создание таблицы tracks_of_digest
    CREATE TABLE IF NOT EXISTS tracks_of_digest (
 	  track_id INT NOT NULL,
@@ -52,5 +54,4 @@
 	  digest_id INT NOT NULL,
     CONSTRAINT fk_digest_id FOREIGN KEY (digest_id) REFERENCES digest (id),
 	  PRIMARY KEY (track_id, digest_id)
-);
-
+   );
